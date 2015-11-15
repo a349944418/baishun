48205a8a16bc73f41a7f00b946695574eb4ccfad
home <1jingzhongren@163.com>
2015-11-15 19:29:26 +0800

    index

--- default/Application/Admin/Controller/IndexController.class.php
+++ default/Application/Admin/Controller/IndexController.class.php
@@ -22,8 +22,7 @@ class IndexController extends AdminController {
      */
     public function index(){
         if(UID){
-            $this->meta_title = '管理首页';
-            $this->display();
+            $this->redirect('Config/group');
         } else {
             $this->redirect('Public/login');
         }
