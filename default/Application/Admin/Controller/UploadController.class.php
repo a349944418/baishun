f49929831c6eb089c174f199577f683527879697
zbq <zbq@163.com>
2015-12-03 18:28:04 +0800

    Signed-off-by: zbq <zbq@163.com>

--- default/Application/Admin/Controller/UploadController.class.php
+++ default/Application/Admin/Controller/UploadController.class.php
@@ -50,6 +50,7 @@ class UploadController extends AdminController
 	    $upload->savePath  =     'Video/'; // 设置附件上传（子）目录
 	    // 上传文件 
 	    $info   =   $upload->upload();
+	    dump($info);
 	    if(!$info) {// 上传错误提示错误信息
 	        $return['error'] = $upload->getError();
 	    }else{// 上传成功
