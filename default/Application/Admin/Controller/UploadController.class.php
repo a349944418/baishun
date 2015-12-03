648a90c516548de940b8c7bfec13fbcd33da86f9
zbq <zbq@163.com>
2015-12-03 17:06:16 +0800

    Signed-off-by: zbq <zbq@163.com>

--- default/Application/Admin/Controller/UploadController.class.php
+++ default/Application/Admin/Controller/UploadController.class.php
@@ -44,7 +44,7 @@ class UploadController extends AdminController
 	{
 		$return = array('error'=>'');
 		$upload = new \Think\Upload();// 实例化上传类
-	    $upload->maxSize   =     5242880 ;// 设置附件上传大小
+	    $upload->maxSize   =     10485760 ;// 设置附件上传大小
 	    $upload->exts      =     array('mp4');// 设置附件上传类型
 	    $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
 	    $upload->savePath  =     'Video/'; // 设置附件上传（子）目录
