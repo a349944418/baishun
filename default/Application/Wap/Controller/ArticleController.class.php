cfb4faef74f08a9299eb6ab1d2f498e0c14fe287
home <1jingzhongren@163.com>
2015-12-30 19:58:14 +0800

    test

--- default/Application/Wap/Controller/ArticleController.class.php
+++ default/Application/Wap/Controller/ArticleController.class.php
@@ -129,7 +129,7 @@ class ArticleController extends HomeController {
 		/* 更新浏览数 */
 		$map = array('id' => $id);
 		$Document->where($map)->setInc('view');
-
+		dump($category);
 		/* 模板赋值并渲染模板 */
 		$this->assign('category', $category);
 		$this->assign('info', $info);
